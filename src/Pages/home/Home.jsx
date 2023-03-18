3238da2d895528a6b109bbfe0fa34503b5f2c4ac
Punit Mudgal <ssmudgal01@gmail.com>
2023-03-18 14:03:43 +0530

    new home

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
+import Fashion from "../../assets/fashion-white-bg2.jpg";
+// import Fashion from '../../assets/fashion-purple-bg.jpg'
 
 export default function Home() {
   return (
-    <div className='home-full'>
-      Home
+    <>
+      <div className="home-image">
+        <img src={Fashion} alt="fashion-Lady" />
+      {/* black gradient */}
+      <div className="gradient" />
       </div>
-  )
+
+      {/* text */}
+      <div className="home-content">
+        <p>SPRING / SUMMER COLLECTION 2023</p>
+        <h1>
+          Get up to 30% Off
+          <br /> New Arrivals
+        </h1>
+        <a href="/">SHOP NOW</a>
+      </div>
+
+    </>
+  );
 }
