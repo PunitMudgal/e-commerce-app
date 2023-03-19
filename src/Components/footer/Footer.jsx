8c85e8a566077e1cd920361351d874f459da9774
Punit Mudgal <ssmudgal01@gmail.com>
2023-03-19 13:28:15 +0530

    footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {FaInstagram, FaLinkedinIn, FaGithub, FaTwitter} from 'react-icons/fa';
+import './footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-full section-padding'>
+        <div className='footer-content'>
+          <h5>FASHION<span>CLUB</span></h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+          <p className='footer-content-copyright'>
+          &#169; PUNIT_SHARMA ALL RIGHTS RESERVED.
+          </p>
+        </div>
+        <div className='footer-icons'>
+          <FaInstagram className='social-icon' size={45}/>
+          <FaTwitter className='social-icon' size={45} />
+          <FaGithub className='social-icon' size={45}/>
+          <FaLinkedinIn className='social-icon' size={45} />
+        </div>
+    </div>
   )
 }
