fe6e400b8c84acad3e378e3ddee37886cdbda0b4
Punit Mudgal <ssmudgal01@gmail.com>
2023-03-19 19:25:21 +0530

    adding errorPage

--- src/Pages/errorPage/ErrorPage.jsx
+++ src/Pages/errorPage/ErrorPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import './errorPage.css'
+import errorPng from '../../assets/404.jpg'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div className='errorpage'>
+      <img src={errorPng} alt="404" />
+    </div>
   )
 }
